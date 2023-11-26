7121d5b5227a9ab9904b9d6cc63cf2754129d426
Malte Weber <malte.weber@uni-jena.de>
2023-11-26 20:32:10 +0100

    add rating to question

--- src/app/domain/questions/models.py
+++ src/app/domain/questions/models.py
@@ -1,9 +1,14 @@
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+Rating = Annotated[int, Field(gt=0, le=5)]
 
 
 class Question(BaseModel):
     id: UUID | None
     question: str
     version: int
+    rating: Rating
+
