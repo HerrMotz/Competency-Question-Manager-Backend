30c4cc97d100247160ab67720225b0a14ce51307
se72gig <dominik.buschold@uni-jena.de>
2023-11-26 21:29:21 +0100

    add auth to user controller and app

--- src/app/domain/accounts/controllers/user_controller.py
+++ src/app/domain/accounts/controllers/user_controller.py
@@ -1,22 +1,28 @@
 from typing import Annotated, TypeVar
 from uuid import UUID
 
-from litestar import Controller, delete, get, post, put
+from litestar import Controller, Response, delete, get, post, put
 from litestar.enums import RequestEncodingType
-from litestar.params import Body
+from litestar.params import Body, Dependency
 from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 
-from ..dtos import UserGetDTO, UserRegisterDTO, UserUpdate0DTO
-from ..exceptions import UserNotFoundException
-from ..guards import admin_guard
-from ..services import UserService
+from ..authentication.middleware import AuthenticationMiddleware
+from ..dtos import UserGetDTO, UserLoginDTO, UserRegisterDTO, UserUpdateDTO
+from ..exceptions import (
+    UnmatchedCredentialsException,
+    UserNotFoundException,
+    VerificationRequiredException,
+)
+from ..guards import system_admin_guard
+from ..services import MOCK_USER_SERVICE
 
 T = TypeVar("T")
 JsonEncoded = Annotated[T, Body(media_type=RequestEncodingType.JSON)]
+AuthenticationDependency = Annotated[AuthenticationMiddleware, Dependency(skip_validation=True)]
 
 
 class UserController(Controller):
-    user_service = UserService()
+    user_service = MOCK_USER_SERVICE
     path = "/users"
     tags = ["User"]
 
@@ -32,14 +38,14 @@ class UserController(Controller):
             return user
         raise UserNotFoundException(user_id)
 
-    @put("/{user_id:uuid}", guards=[admin_guard], status_code=HTTP_200_OK)
-    async def update_user_handler(self, user_id: UUID, data: JsonEncoded[UserUpdate0DTO]) -> UserGetDTO:
+    @put("/{user_id:uuid}", guards=[system_admin_guard], status_code=HTTP_200_OK)
+    async def update_user_handler(self, user_id: UUID, data: JsonEncoded[UserUpdateDTO]) -> UserGetDTO:
         """Updates a specific `User`."""
         if user := await self.user_service.update_user(user_id, data):
             return user
         raise UserNotFoundException(user_id)
 
-    @delete("/{user_id:uuid}", guards=[admin_guard], status_code=HTTP_204_NO_CONTENT)
+    @delete("/{user_id:uuid}", guards=[system_admin_guard], status_code=HTTP_204_NO_CONTENT)
     async def delete_user_handler(self, user_id: UUID) -> None:
         """Deletes a specific `User`."""
         if _ := await self.user_service.delete_user(user_id):
@@ -51,7 +57,7 @@ class UserController(Controller):
         """Registers a new `User`."""
         return await self.user_service.add_user(data)
 
-    @put("/verify/{user_id:uuid}", guards=[admin_guard], status_code=HTTP_200_OK)
+    @put("/verify/{user_id:uuid}", guards=[system_admin_guard], status_code=HTTP_200_OK)
     async def verify_user_handler(self, user_id: UUID) -> UserGetDTO:
         """Verifies a specific `User`."""
         if user := await self.user_service.verify_user(user_id):
@@ -59,11 +65,10 @@ class UserController(Controller):
         raise UserNotFoundException(user_id)
 
     @post("/login", status_code=HTTP_200_OK)
-    async def login_handler(self) -> None:
-        """Not yet implemented."""
-        ...
-
-    @get("/logout", status_code=HTTP_200_OK)
-    async def logout_handler(self) -> None:
-        """Not yet implemented."""
-        ...
+    async def login_handler(self, authenticator: AuthenticationDependency, data: JsonEncoded[UserLoginDTO]) -> Response[UserGetDTO]:
+        """Returns a valid authentication header given a matching set of credentials."""
+        if user := await self.user_service.get_user_by_credentials(data):
+            if user.is_verified:
+                return authenticator.login(user)
+            raise VerificationRequiredException()
+        raise UnmatchedCredentialsException()
