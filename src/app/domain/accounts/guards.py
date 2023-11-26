38d02ac354615ea7e1739a2c7341b09a52b4735f
se72gig <dominik.buschold@uni-jena.de>
2023-11-26 21:29:00 +0100

    add basic guards and related exceptions

--- src/app/domain/accounts/guards.py
+++ src/app/domain/accounts/guards.py
@@ -1,27 +1,57 @@
-from typing import Any
+# pyright: reportUnusedVariable=false
+
+from typing import Any, Type, TypeVar
+from uuid import UUID
 
 from litestar.connection import ASGIConnection
-from litestar.exceptions import NotAuthorizedException
+from litestar.exceptions import ImproperlyConfiguredException
 from litestar.handlers.base import BaseRouteHandler
 
+from .exceptions import (
+    GroupMembershipRequiredException,
+    ProjectManagerRequiredException,
+    ProjectMembershipRequiredException,
+    SystemAdministratorRequiredException,
+    VerificationRequiredException,
+)
 from .models import User
 
+T = TypeVar("T")
 
-def user_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+
+def _get_path_param(_: Type[T], param: str, connection: ASGIConnection[Any, Any, Any, Any]) -> T | None:
+    """Walruses can't write type hints and functions lack support for generics (just pre 3.12 things)."""
+    return connection.path_params.get(param, None)
+
+
+async def user_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
     """Limit route access to verified users only."""
+
+    # TODO: user guard will be fulfilled by authentication middleware
+
     if not connection.user.is_verified:
-        raise NotAuthorizedException("This route may only be accessed by verified users. Contact your administrators to get your account verified.")
+        raise VerificationRequiredException()
 
 
-def admin_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
-    """Limit route access to admins only."""
-    return
+async def system_admin_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+    """Limit route access to system admins only."""
+    if not connection.user.is_system_admin:
+        raise SystemAdministratorRequiredException()
 
-    # TODO:
-    # * implement user authentication
 
-    if not connection.user.is_admin:
-        raise NotAuthorizedException("This route may only be accessed by an admin.")
+async def project_manager_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+    """Limit route access to project managers only.
+
+    Requires a `project_id: UUID` path parameter to be set.
+    """
+
+    if project_id := _get_path_param(UUID, "project_id", connection):
+        ...
+        # 1. get project
+        # 2. return if user is project manager, else raise
+
+        raise ProjectManagerRequiredException()
+    raise ImproperlyConfiguredException()
 
 
 # TODO:
@@ -31,11 +61,42 @@ def admin_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHan
 #   or should we move this stuff to a separate domain?
 
 
-def group_member_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
-    """Limit route access to group members only."""
-    ...
+# TODO: are roles hierarchial (i.e. sys admin can do anything an project manager can)?
 
 
-def ontology_engineer_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+async def project_member_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+    """Limit route access to project members (i.e. members of groups within a project) only.
+
+    Requires a `project_id: UUID` path parameter to be set.
+    """
+
+    if project_id := _get_path_param(UUID, "project_id", connection):
+        ...
+        # 1. get project
+        # 2. return if user is in any project group, else raise
+
+        raise ProjectMembershipRequiredException()
+    raise ImproperlyConfiguredException()
+
+
+async def group_member_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
+    """Limit route access to group members only.
+
+    Requires a `project_id: UUID` and `group_id: UUID` path parameter to be set.
+    """
+
+    if project_id := _get_path_param(UUID, "project_id", connection):
+        if group_id := _get_path_param(UUID, "group_id", connection):
+            ...
+            # 1. get project
+            # 2. get group
+            # (3.) validate that group is part of project
+            # 4. return if user is in group, else raise
+
+            raise GroupMembershipRequiredException()
+    raise ImproperlyConfiguredException()
+
+
+async def ontology_engineer_guard(connection: ASGIConnection[Any, User, Any, Any], _: BaseRouteHandler) -> None:
     """Limit route access to ontology engineers only."""
     ...
