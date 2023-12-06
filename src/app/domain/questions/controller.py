24c616b506bf0c3d9aead64180d13b1def260cf9
Malte Weber <malte.weber@uni-jena.de>
2023-12-06 20:50:52 +0100

    Refactor Questions codebase using SQLAlchemy and DTOs

--- src/app/domain/questions/controller.py
+++ src/app/domain/questions/controller.py
@@ -1,10 +1,11 @@
-import logging
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from litestar import Controller, post, get, delete
 from litestar.exceptions import HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from .models import Question
+from .models import Question, QuestionDTO
 
 
 class QuestionController(Controller):
@@ -12,26 +13,56 @@ class QuestionController(Controller):
     mock_db: dict[UUID, Question] = {}
 
     @post("/")
-    async def create_question(self, data: Question) -> Question:
-        question_id = uuid4()
-        self.mock_db[question_id] = Question(
-            id=question_id, question=data.question, version=data.version, rating=data.rating
-        )
-        return self.mock_db.get(question_id)
+    async def create_question(self, session: AsyncSession, data: QuestionDTO) -> QuestionDTO:
+        """
+        Creates a question in the session.
 
-    @get()
-    async def list_questions(self) -> list[Question]:
-        return list(self.mock_db.values())
+        :param session: The session object to use for database operations.
+        :param data: The question data to be created.
+        :return: The created question data.
+        """
+        question = Question(question=data.question, version=data.version)
+        session.add(question)
+        await session.commit()
+        return data
+
+    @get("/")
+    async def get_questions(self, session: AsyncSession) -> list[QuestionDTO]:
+        """
+        :param session: AsyncSession object used to execute the database query and retrieve questions.
+        :return: A list of QuestionDTO objects representing the retrieved questions.
+        """
+        questions = await session.execute(select(Question))
+        return [QuestionDTO.model_validate(question) for question in questions.scalars()]
 
     @get(path="/{question_id:uuid}")
-    async def get_question(self, question_id: UUID) -> Question:
-        if question_id in self.mock_db:
-            return self.mock_db[question_id]
-        raise HTTPException(status_code=404, detail="Question not found")
+    async def get_question(self, session: AsyncSession, question_id: UUID) -> QuestionDTO:
+        """
+        Retrieves a question by its ID.
+
+        :param session: An `AsyncSession` object representing the database session.
+        :param question_id: A `UUID` object representing the ID of the question to retrieve.
+        :return: A `QuestionDTO` object containing the retrieved question.
+        :raises HTTPException: If the question with the specified ID is not found.
+        """
+        question = await session.execute(select(Question).where(Question.id == question_id))
+        if not question.scalar():
+            raise HTTPException(status_code=404, detail="Question not found")
+        return QuestionDTO.model_validate(question.scalar())
 
     @delete(path="/{question_id:uuid}")
-    async def delete_question(self, question_id: UUID) -> None:
-        if question_id in self.mock_db:
-            self.mock_db.pop(question_id)
-        else:
+    async def delete_question(self, session: AsyncSession, question_id: UUID) -> None:
+        """
+        Deletes a question from the database.
+
+        :param session: The async session used to interact with the database.
+        :param question_id: The UUID of the question to be deleted.
+        :return: None
+
+        :raises HTTPException: If the question with the specified ID is not found.
+        """
+        question = await session.execute(select(Question).where(Question.id == question_id))
+        if not question.scalar():
             raise HTTPException(status_code=404, detail="Question not found")
+        await session.delete(question.scalar())
+        await session.commit()
--- src/app/domain/questions/models.py
+++ src/app/domain/questions/models.py
@@ -1,14 +1,23 @@
 from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import Field
+from lib.dto import BaseModel
+
+from litestar.contrib.sqlalchemy.base import UUIDAuditBase
+from sqlalchemy.orm import Mapped
+
 
 Rating = Annotated[int, Field(gt=0, le=5)]
 
 
-class Question(BaseModel):
+class QuestionDTO(BaseModel):
     id: UUID | None = None
     question: str
     version: int
-    rating: Rating
+    rating: Rating | None = None
 
+
+class Question(UUIDAuditBase):
+    question: Mapped[str]
+    version: Mapped[int]
