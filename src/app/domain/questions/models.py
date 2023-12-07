51cda98b44aff96b74bde424fac48c8681ac97f8
Malte Weber <malte.weber@uni-jena.de>
2023-12-07 21:43:30 +0100

    etend Question model with author and author id

--- src/app/domain/questions/models.py
+++ src/app/domain/questions/models.py
@@ -1,11 +1,9 @@
 from uuid import UUID
-
-from pydantic import Field
 from lib.dto import BaseModel
 
 from litestar.contrib.sqlalchemy.base import UUIDAuditBase
-from sqlalchemy.orm import Mapped
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from ..rating.models import Rating
 
@@ -15,8 +13,11 @@ class QuestionDTO(BaseModel):
     question: str
     version: int
     rating: Rating | None = None
+    author_id: UUID | None = None
 
 
 class Question(UUIDAuditBase):
     question: Mapped[str]
     version: Mapped[int]
+    author_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+    author = relationship("User", back_populates="questions")
