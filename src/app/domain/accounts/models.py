365b0bb80dd14e61578573cfbb579bd782388067
se72gig <dominik.buschold@uni-jena.de>
2023-12-11 00:45:44 +0100

    add project and group model, update user model

--- src/app/domain/accounts/models.py
+++ src/app/domain/accounts/models.py
@@ -1,19 +1,28 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from litestar.contrib.sqlalchemy.base import UUIDAuditBase
 from sqlalchemy import LargeBinary
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from domain.groups.models import Group
+    from domain.projects.models import Project
 
 
 class User(UUIDAuditBase):
-    email: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
     name: Mapped[str] = mapped_column(unique=True)
     password_hash: Mapped[bytes] = mapped_column(LargeBinary(length=128))
     password_salt: Mapped[bytes] = mapped_column(LargeBinary(length=128))
     is_system_admin: Mapped[bool]
     is_verified: Mapped[bool]
 
+    managed_projects: Mapped[list[Project]] = relationship(secondary="project_managers", back_populates="managers")
+    joined_groups: Mapped[list[Group]] = relationship(secondary="group_members", back_populates="members")
+
     # TODO: add relationships
-    # projects: Mapped[list["Project"]] = relationship(back_populates="members")
-    # groups: Mapped[list["Group"]] = relationship(back_populates="members")
     # questions: Mapped[list["Question"]] = relationship(back_populates="author")
     # comments: Mapped[list["Comment"]] = relationship(back_populates="author")
     # ratings: Mapped[list["Rating"]] = relationship(back_populates="author")
