dee9592655157c0da9fada574f406acca06ba65c
Malte Weber <malte.weber@uni-jena.de>
2023-12-11 22:17:26 +0100

    add dto for ratings

--- src/app/domain/rating/models.py
+++ src/app/domain/rating/models.py
@@ -1,14 +1,26 @@
 from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from advanced_alchemy.base import UUIDAuditBase
+from lib.dto import BaseModel
+from pydantic import Field
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 IndividualRating = Annotated[int, Field(gt=0, le=5)]
 
 
-class Rating(BaseModel):
+class RatingDTO(BaseModel):
     id: UUID | None = None
     rating: IndividualRating
     question_id: UUID
     user_id: UUID | None = None
-    version: int
+
+
+class Rating(UUIDAuditBase):
+    rating: Mapped[int]
+    question_id: Mapped[UUID] = mapped_column(ForeignKey("question.id"))
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
+    user = relationship("User", back_populates="ratings")
+    question = relationship("Question", back_populates="ratings")
+
