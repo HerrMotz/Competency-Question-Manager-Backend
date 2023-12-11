335c320588446a03877f980e8a963ffaaba7143a
Malte Weber <malte.weber@uni-jena.de>
2023-12-11 22:17:06 +0100

    add several dtos for questions

--- src/app/domain/questions/models.py
+++ src/app/domain/questions/models.py
@@ -1,23 +1,51 @@
 from uuid import UUID
-from lib.dto import BaseModel
 
+from lib.dto import BaseModel
 from litestar.contrib.sqlalchemy.base import UUIDAuditBase
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
-from ..rating.models import Rating
+from ..rating.models import RatingDTO
 
 
-class QuestionDTO(BaseModel):
-    id: UUID | None = None
+class QuestionOverviewDTO(BaseModel):
+    id: UUID
     question: str
-    version: int
-    rating: Rating | None = None
-    author_id: UUID | None = None
+    rating: int | None = None
+    author_id: UUID
+    author_name: str
+
+
+class QuestionDetailDTO(BaseModel):
+    id: UUID
+    question: str
+    ratings: list[RatingDTO]
+    author_name: str
+    author_id: UUID
+
+
+class QuestionCreateDTO(BaseModel):
+    question: str
+
+
+class QuestionCreatedDTO(BaseModel):
+    id: UUID
+    question: str
+    author_id: UUID
+
+
+class QuestionUpdateDTO(BaseModel):
+    question: str
+
+
+class QuestionUpdatedDTO(BaseModel):
+    id: UUID
+    question: str
+    author_id: UUID
 
 
 class Question(UUIDAuditBase):
     question: Mapped[str]
-    version: Mapped[int]
-    author_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+    author_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
     author = relationship("User", back_populates="questions")
+    ratings: Mapped[list["Rating"]] = relationship(back_populates="question")
