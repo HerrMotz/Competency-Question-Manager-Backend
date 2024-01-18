67571f36209f3c8ad7f288c1d7af630f72aa434e
Malte Weber <malte.weber@uni-jena.de>
2024-01-18 21:13:19 +0100

    use new dtos in questions controller

--- src/app/domain/questions/controller.py
+++ src/app/domain/questions/controller.py
@@ -1,70 +1,70 @@
-import math
-from typing import Any
+from typing import Annotated, Any, Sequence, TypeVar
 from uuid import UUID, uuid4
 
 from litestar import Controller, Request, delete, get, post
+from litestar.enums import RequestEncodingType
 from litestar.exceptions import HTTPException
+from litestar.params import Body
 from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from ..accounts.models import User
-from ..rating.dtos import RatingGetDTO
-from ..rating.models import IndividualRating, Rating
-from ..rating.services import RatingService
-from .models import (
-    Question,
-    QuestionCreatedDTO,
+from .dtos import (
+    QuestionCreate,
     QuestionCreateDTO,
     QuestionDetailDTO,
     QuestionOverviewDTO,
 )
+from .models import Question
+from ..accounts.models import User
+from ..rating.models import Rating
+
+T = TypeVar("T")
+JsonEncoded = Annotated[T, Body(media_type=RequestEncodingType.JSON)]
 
 
 class QuestionController(Controller):
     path = "/questions/"
     tags = ["Questions"]
-    rating_service = RatingService()
 
-    @post("/", status_code=HTTP_201_CREATED)
+    @post("/", dto=QuestionCreateDTO, return_dto=QuestionDetailDTO, status_code=HTTP_201_CREATED)
     async def create_question(
-        self, session: AsyncSession, data: QuestionCreateDTO, request: Request[User, Any, Any]
-    ) -> QuestionCreatedDTO:
+        self, session: AsyncSession, data: JsonEncoded[QuestionCreate], request: Request[User, Any, Any]
+    ) -> Question:
         """
-        Creates a question in the session.
+        Creates a new `Question`
 
+        :param request: Request[User, Any, Any]
         :param session: The session object to use for database operations.
         :param data: The question data to be created.
         :return: The created question data.
         """
         question = Question(id=uuid4(), question=data.question, author_id=request.user.id)
         session.add(question)
-        return QuestionCreatedDTO.model_validate(question)
+        await session.commit()
+        await session.refresh(question)
+        return await session.scalar(
+            select(Question)
+            .where(Question.id == question.id)
+            .options(selectinload(Question.author))
+            .options(selectinload(Question.ratings))
+        )
 
-    @get("/", status_code=HTTP_200_OK)
-    async def get_questions(self, session: AsyncSession) -> list[QuestionOverviewDTO]:
+    @get("/", return_dto=QuestionOverviewDTO, status_code=HTTP_200_OK)
+    async def get_questions(self, session: AsyncSession) -> Sequence[Question]:
         """
         :param session: AsyncSession object used to execute the database query and retrieve questions.
         :return: A list of QuestionDTO objects representing the retrieved questions.
         """
-        questions = await session.scalars(
-            select(Question).options(selectinload(Question.author)).options(selectinload(Question.ratings))
-        )
-        question_dtos = [
-            QuestionOverviewDTO(
-                id=q.id,
-                question=q.question,
-                author_id=q.author_id,
-                author_name=q.author.name,
-                rating=int(sum([rating.rating for rating in q.ratings]) / len(q.ratings)) if len(q.ratings) > 0 else 0,
+        return (
+            await session.scalars(
+                select(Question).options(selectinload(Question.author)).options(selectinload(Question.ratings))
             )
-            for q in questions.all()
-        ]
-        return question_dtos
+        ).all()
 
-    @get(path="/{question_id:uuid}", status_code=HTTP_200_OK)
-    async def get_question(self, session: AsyncSession, question_id: UUID) -> QuestionDetailDTO:
+    @get(path="/{question_id:uuid}", return_dto=QuestionDetailDTO, status_code=HTTP_200_OK)
+    async def get_question(self, session: AsyncSession, question_id: UUID) -> Question:
         """
         Retrieves a question by its ID.
 
@@ -74,35 +74,17 @@ class QuestionController(Controller):
         :raises HTTPException: If the question with the specified ID is not found.
         """
 
-        q = await session.scalar(
+        question = await session.scalar(
             select(Question)
             .where(Question.id == question_id)
             .options(selectinload(Question.author))
             .options(selectinload(Question.ratings).options(selectinload(Rating.user)))
-
         )
 
-        if not q:
+        if not question:
             raise HTTPException(status_code=404, detail="Question not found")
 
-        ratings = [
-            RatingGetDTO(
-                rating=rating.rating,
-                user_id=rating.user_id,
-                user_name=rating.user.name,
-                question_id=rating.question_id,
-            )
-            for rating in q.ratings
-        ]
-        question_dto = QuestionDetailDTO(
-            id=q.id,
-            question=q.question,
-            ratings=ratings,
-            rating=int(sum([rating.rating for rating in ratings]) / len(ratings)) if len(ratings) > 0 else 0,
-            author_id=q.author_id,
-            author_name=q.author.name,
-        )
-        return question_dto
+        return question
 
     @delete(path="/{question_id:uuid}", status_code=HTTP_204_NO_CONTENT)
     async def delete_question(self, session: AsyncSession, question_id: UUID) -> None:
