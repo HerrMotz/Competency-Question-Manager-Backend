3325abbbde6efebf78fbf363c576ad2e6c5c3c39
se72gig <dominik.buschold@uni-jena.de>
2024-01-19 18:07:44 +0100

    add project manager to group middleware

--- src/app/domain/groups/middleware.py
+++ src/app/domain/groups/middleware.py
@@ -12,7 +12,7 @@ T = TypeVar("T")
 class UserGroupPermissionsMiddleware(AbstractUserPermissionsMiddleware):
     """Handles group permission headers when accessing group routes."""
 
-    _headers = ["Permissions-Group-Member"]
+    _headers = ["Permissions-Group-Member", "Permissions-Project-Manager"]
 
     @property
     def param_name(self) -> str:
@@ -20,3 +20,4 @@ class UserGroupPermissionsMiddleware(AbstractUserPermissionsMiddleware):
 
     async def set_headers(self, headers: MutableScopeHeaders, session: AsyncSession, id: UUID, user_id: UUID) -> None:
         headers[self._headers[0]] = str(await GroupService.is_member(session, id, user_id))
+        headers[self._headers[1]] = str(await GroupService.is_manager(session, id, user_id))
