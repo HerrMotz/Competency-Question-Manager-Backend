b2a4a6d32e8f553f454346248e182dc47828ffd6
se72gig <dominik.buschold@uni-jena.de>
2024-01-25 19:19:10 +0100

    optional description for put

--- src/app/domain/projects/models.py
+++ src/app/domain/projects/models.py
@@ -12,6 +12,7 @@ if TYPE_CHECKING:
     from domain.accounts.models import User
     from domain.groups.models import Group
     from domain.consolidations.models import Consolidation
+    
 ProjectManagers = Table(
     "project_managers",
     UUIDAuditBase.metadata,
