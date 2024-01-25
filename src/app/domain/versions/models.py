80851814297fce7edaffb37170d725fa0d193ea3
Malte Weber <malte.weber@uni-jena.de>
2024-01-25 21:05:22 +0100

    change Versions data model

--- src/app/domain/versions/models.py
+++ src/app/domain/versions/models.py
@@ -3,24 +3,18 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from uuid import UUID
 
+
 from litestar.contrib.sqlalchemy.base import UUIDAuditBase
-from sqlalchemy import Column, Table
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.schema import ForeignKey
 
 if TYPE_CHECKING:
     from domain.questions.models import Question
 
 
-VersionQuestions = Table(
-    "version_questions",
-    UUIDAuditBase.metadata,
-    Column[UUID]("version_id", ForeignKey("version.id"), primary_key=True),
-    Column[UUID]("question_id", ForeignKey("question.id"), primary_key=True),
-)
-
-
 class Version(UUIDAuditBase):
-    questions: Mapped[list[Question]] = relationship(
-        secondary="version_questions", back_populates="versions"
-    )
+    question_string: Mapped[str]
+    version_number: Mapped[int]
+
+    question_id: Mapped[UUID] = mapped_column(ForeignKey("question.id"))
+    question: Mapped[Question] = relationship(back_populates="versions")
